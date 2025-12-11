33fea07e044657e6666683465560104cb6e5ba82
pcardel1205 <pcardel1205@g.educaand.es>
2025-12-11 12:46:31 +0100

    primera parte del js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                        :header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SWIPER PRODUCTS ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/
+
+
+/*=============== DARK LIGHT THEME ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
